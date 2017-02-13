a4e855083da0084e65d9d37d1cd60c850aba9bd7
benBen <benmbenjamin@gmail.com>
2017-02-12 23:06:07 -0800

    (pseudo)pseudocode

--- src/all.js
+++ src/all.js
@@ -43,6 +43,8 @@ app.component('portfolio', {
 
     $scope.test = 'portfolio testing';
 
+    // why does this appear to work /
+    // trigger rerender only 90% of the time?
     setTimeout(() => {
       $scope.bish = 'bish';
     }, 1000);
@@ -53,18 +55,7 @@ app.component('portfolio', {
       console.log($scope.bloobs);
     });
 
-    // const randoBg = async () => {
-    //   try {
-    //     let request = await fetch('http://thecatapi.com/api/images/get?format=src&type=gif');
-    //     $scope.bg = request.url;
-    //     console.log("scope bg", $scope.bg);
-    //     return request.url;
-    //   }
-    //   catch (error) {
-    //     console.log("fawk", error);
-    //   }
-    // };
-
+    // in reality this will be pulled from WordpressData
     const getBg = url => {
       console.log("getting guy");
       $http({
@@ -80,6 +71,34 @@ app.component('portfolio', {
     };
 
     getBg('http://thecatapi.com/api/images/get?format=src&type=gif');
+
+    // WordpressData should pass in the index/key of
+    // the current portfolio client, along with the key
+    // of the previous + next clients for the nav buttons.
+    // Question: will we use anchor tags for those buttons
+    // or is there an ng-element that can update state /
+    // alert $routeProvider to rerender component without
+    // refresh (similar to <Link/> in react)?
+
+    // content: needs to be structured like a standard
+    // Wordpress post, basically a glob of HTML; this is
+    // because for some clients they'll wanna have photos,
+    // a description, bulleted list who knows wtf what. So
+    // I think the ideal JSON would look like:
+    // Data{
+    //   parris: {
+    //     title: "Paris Goebel",
+    //     shortTitle: "Parris",
+    //     bgImg: "http://wethem.us/path/to/wp-content/media-id/hashtag/awful/path",
+    //     content: "<div class='arbitrary-wp-class'>stuff</div>",
+    //     ...
+    //   },
+    //   zayn: {...},
+    //   ...
+    // }
+
+    // would this^ be something that I need to specify in
+    // functions.php or wherever we configure wp-json?
   }]
 });
 app.component('sidenav', {
